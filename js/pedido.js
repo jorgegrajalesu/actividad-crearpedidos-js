538faf8fdfa6de7cd8768682f88232ac8bb41b68
jorgegrajalesu <jegosystem@gmail.com>
2023-09-08 09:36:28 -0500

    080923

---
+++ js/pedido.js
@@ -0,0 +1,25 @@
+// crear un objeto de window
+window.onload = function() {
+    // crer un objeto literal
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre:'Hotdog',
+            precio:4000,
+            imagen:'/images/hotdog.jpg'
+        },
+        {
+            id: 2,
+            nombre:'Hamburguesa',
+            precio:5000,
+            imagen:'/images/hamburger.jpg'
+        },
+        {
+            id: 1,
+            nombre:'Pizza',
+            precio:8000,
+            imagen:'/images/pizza.jpg'
+        },
+    ]
+};
+
