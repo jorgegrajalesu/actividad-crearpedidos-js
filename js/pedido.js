534ca758601970b077896c97b5471358cb45c3a0
jorgegrajalesu <jegosystem@gmail.com>
2023-09-13 09:49:23 -0500

    130923

--- js/pedido.js
+++ js/pedido.js
@@ -41,7 +41,85 @@ const milocalStorage = window.localStorage;
 function renderizarProductos() {
     // recorrer la base de datos con un foreach
     baseDeDatos.forEach((info) => {
-        // estructura 
+        // estructura para crear los elementos para el html
+        const miNodo = document.createElement('div');
+        miNodo.classList.add('card', 'col-sm-4');
+        // body
+        const miNodoCardBody= document.createElement('div');
+        miNodoCardBody.classList.add('card-body');
+        // titulo
+        const miNodoTitle = document.createElement('h5');
+        miNodoTitle.classList.add('card-title');
+        // llamar de la base de datos el titulo
+        miNodoTitle.textContent = info.nombre;
+        // imagen
+        const miNodoImagen = document.createElement('img');
+        miNodoImagen.classList.add('img-fluid');
+        // llamar la imagen de la base de datos
+        miNodoImagen.setAttribute('src', info.imagen);
+        // precio
+        const miNodoPrecio = document.createElement('p');
+        miNodoPrecio.classList.add('card-text');
+        // llamar el precio de la base de datos
+        miNodoPrecio.textContent = '$' + info.precio;
+
+        // boton y su evento
+        const miNodoBoton = document.createElement('button');
+        miNodoBoton.classList.add('btn', 'btn-primary');
+        miNodoBoton.textContent = 'Agregar';
+        miNodoBoton.setAttribute('marcador',info.id);
+        miNodoBoton.addEventListener('click', agregarProducto);
+
+        // insertar los elementos de la base de datos a sus respectivas etiquetas html
+        miNodoCardBody.appendChild(miNodoImagen);
+        miNodoCardBody.appendChild(miNodoTitle);
+        miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoBoton);
+        miNodo.appendChild(miNodoCardBody);
+        DOMitems.appendChild(miNodo);
+
+    
+    });
+    
+}
+
+// Crear la función para el boton con su evento click, para agregar los produductos al pedido
+/**
+ * 
+ * @param {eventoClick} evento click para el boton agregar
+ */
+function agregarProducto(evento) {
+    // tener presente la etiqueta ul con id=Pedido, en el index, para agregar el producto
+    Pedido.push(evento.target.setAttribute('marcador'));
+}
+
+// crear la función para el pedido
+function renderizarPedido() {
+    // limpiar todo el los items en el html
+    DOMPedido.textContent = '';
+    // quitar los duplicados
+    const PedidoSinDuplicados = [...new Set(Pedido)];
+    // generar el pedido de acuerdo a los items
+    PedidoSinDuplicados.forEach((item) => {
+        // obtenemos los items de la base de datos
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+            
+        });
+        // contar el numeros de veces que se repite el producto
+        const numeroUnidadesItem = Pedido.reduce((total, itemId) => {
+            // si coinciden los id, incremento
+            return itemId === item ? total += 1 : total;            
+        }, 0);
+        // crear la lista de los items del pedido
+        const miNodo = document.createElement('li');
+        miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+        miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}`;
+
+
+
+
+
     });
     
 }
